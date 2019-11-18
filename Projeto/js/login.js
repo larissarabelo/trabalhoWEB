c2d71e2ff0ba5aa4f9aa59ec9e175f3f0da62b8a
larissarabelo <56235344+larissarabelo@users.noreply.github.com>
2019-11-18 17:06:03 -0300

    Cadastro e autenticação
    Duda ajudou

---
+++ Projeto/js/login.js
@@ -0,0 +1,44 @@
+$(document).ready(function(){
+    $("#bCadastrar").click(function(){
+        window.location.href="paginas/cadastro.html";
+    });
+    $("input").attr("required",true);
+    $("form#formularioLogin").submit(function(){
+        preenchido = true;
+        $("input").each(function(id,input){
+            if($(input).val()==""){
+                preenchido = false
+            }
+        });
+        if(preenchido){
+            $.ajax({
+                type:"post",
+                dataType:"json",
+                url:"./php/login.php",
+                data:{
+                    "email":$("#email").val(),
+                    "senha":$("#senha").val()
+                },
+                success:function(retorno){
+                    switch(retorno){
+                        case 1:
+                            window.location.href="./paginas/entrada.html";
+                            break;
+                        case -1:
+                            alert("Acesso não autorizado.");
+                            break;
+                        case -2:
+                            window.location.href="../paginas/cadastro.html";
+                            break;
+                        case -3:
+                            alert("Login ou Senha Incorretos.")
+                            break;
+                    }
+                }
+            });
+        }else{
+            alert("Preencha todos os campos.");
+        }
+        return false;
+    });
+});
