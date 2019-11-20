c230de100d520dd1cf1e588a16453eaa2e9e068d
JulianoOB <55262443+JulianoOB@users.noreply.github.com>
2019-11-20 20:33:21 -0300

    tabela

--- Projeto/js/email.js
+++ Projeto/js/email.js
@@ -5,27 +5,24 @@ $(document).ready(function(){
         $.ajax({
             type:"post",
             dataType:"json",
-            url:"./php/receber.php",
+            url:"../php/mandar.php",
             data:{
                 "remetente": remetente,
-                "asunto": asunto,
+                "assunto": assunto,
                 "meensagem": mensagem
             },
             success:function(retorno){
                 for(e=0; e<=retorno.leight; e++){
-                    var table = document.getElementById("emails");
-                    var row = table.insertRow(e);
-                    row.innerHTML = "<tr><td>"+
+                    $("#mensagens").append("<tr><td>"+
                     remetente+      "</td><td>"+
                     assunto+        "</td><td>"+
-                    mensagem+       "</td></tr>";
+                    mensagem+       "</td></tr>");
                 };
             }
         });
     });
 
     $("#enviados").click(function(){
-
 
     });
 
